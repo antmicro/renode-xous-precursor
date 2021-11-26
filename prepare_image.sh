ffaaf5d11872f02ff264d9526ad8031fa438395e
Kamil Galik <kgalik@internships.antmicro.com>
2021-11-26 18:02:08 +0100

    Add simple test case (WIP)

--- prepare_image.sh
+++ prepare_image.sh
@@ -1,12 +1,6 @@
 #!/bin/bash  -xeu
 
-# Download xous-core
-git clone https://github.com/betrusted-io/xous-core.git
-pushd xous-core
-cargo xtask
-popd
-
-# Build riscv32imac-unknown-xous toolchain
+# Build riscv32imac-unknown-xous-elf toolchain
 cd $(rustc --print sysroot)
 wget https://github.com/betrusted-io/rust/releases/download/1.56.1.1/riscv32imac-unknown-xous_1.56.1.zip
 rm -rf lib/rustlib/riscv32imac-unknown-xous-elf # Remove any existing version
@@ -14,11 +8,18 @@ unzip *.zip
 rm *.zip
 cd -
 
+# Download xous-core
+git clone https://github.com/betrusted-io/xous-core.git
+pushd xous-core
+cargo xtask
+popd
+
 # Build renode-image for xous-precursor
 pushd xous-core
 cargo xtask renode-image
 popd
 
+cp xous.resc xous-core/emulation/xous.resc
 
 
 
---
+++ xous.robot
@@ -0,0 +1,21 @@
+*** Settings ***
+Suite Setup                   Setup
+Suite Teardown                Teardown
+Test Setup                    Reset Emulation
+Test Teardown                 Test Teardown
+Resource                      ${RENODEKEYWORDS}
+
+*** Variables ***
+${SCRIPT}                     ${CURDIR}/xous-core/emulation/xous.resc
+${CONSOLE}                    sysbus.console
+
+*** Keywords ***
+
+Create Xous Machine
+    Execute Script            ${SCRIPT}
+*** Test Cases ***
+Should Enter Main Loop
+    Create Xous Machine
+    Start Emulation
+    Wait For Line On Uart     status: starting main loop
+
