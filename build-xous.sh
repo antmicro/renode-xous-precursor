0a6bd445a244e0d779e25d628cf936d7f0b57a4d
Nikodem Rabuliński <nrabulinski@antmicro.com>
2023-02-15 12:51:51 +0100

    Updated Rust and Xous-core to latest versions

--- build-xous.sh
+++ build-xous.sh
@@ -1,12 +1,13 @@
-#!/bin/bash  -xeu
+#!/usr/bin/env bash
+set -xeu
 
 # Donwload rust
-curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain=1.58.1
+curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain=1.67.1
 source $HOME/.cargo/env
 
 # Build riscv32imac-unknown-xous-elf toolchain
 cd $(rustc --print sysroot)
-wget https://github.com/betrusted-io/rust/releases/download/1.58.1.1/riscv32imac-unknown-xous_1.58.1.zip
+wget https://github.com/betrusted-io/rust/releases/download/1.67.1.1/riscv32imac-unknown-xous_1.67.1.zip
 rm -rf lib/rustlib/riscv32imac-unknown-xous-elf # Remove any existing version
 unzip *.zip
 rm *.zip
@@ -15,7 +16,7 @@ cd -
 # Download xous-core
 git clone https://github.com/betrusted-io/xous-core.git
 pushd xous-core
-git checkout 317f313
+git checkout 787525fd
 git am ../patches/*
 
 cargo xtask renode-image
